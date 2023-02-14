76002b5fcbf9ca9c2065367973555e55d5a0f473
roee <reisenr96@gmail.com>
2023-02-14 17:32:13 +0200

    Device + Firmware

--- firmware/apple/src/AppleDevice.java
+++ firmware/apple/src/AppleDevice.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+
 public class AppleDevice {
+
+    private String name;
+
+    private String identifier;
+
+    private ArrayList<AppleBoard> boards;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public ArrayList<AppleBoard> getBoards() {
+        return boards;
+    }
 }
