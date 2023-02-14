599d85c0df1c2251e0dce0d9fb3b15a17089c520
roee <reisenr96@gmail.com>
2023-02-14 18:46:49 +0200

    Device list #2

--- console/src/ConsoleDownloader.java
+++ console/src/ConsoleDownloader.java
@@ -1,5 +1,15 @@
-public class ConsoleDownloader {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
 
+public class ConsoleDownloader {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ArrayList<AppleDevice> deviceList;
+        FirmwareInformation firmwareInformation = new FirmwareInformation();
+        System.out.println("Firmware Downloader");
+        System.out.println("Device list:");
+        deviceList = firmwareInformation.getDeviceList();
+        System.out.println("1:");
+        System.out.println("2:");
     }
 }
--- src/FirmwareInformation.java
+++ src/FirmwareInformation.java
@@ -1,7 +1,12 @@
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
+
 
 public class FirmwareInformation {
 
@@ -14,8 +19,17 @@ public class FirmwareInformation {
         infoClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).build();
     }
 
-    public ArrayList<AppleDevice> getDeviceList() {
+    public ArrayList<AppleDevice> getDeviceList() throws IOException, InterruptedException {
         ArrayList<AppleDevice> deviceList = new ArrayList<>();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.ipsw.me/v4/devices"))
+                .header("Accept", "application/json")
+                .GET()
+                .build();
+        HttpResponse<String> response = infoClient.send(request, HttpResponse.BodyHandlers.ofString());
+        
+
+
 
 
         return deviceList;
