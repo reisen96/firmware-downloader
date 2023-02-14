874611dc86e684cbec7c4368f7efcb625af9b7b1
roee <reisenr96@gmail.com>
2023-02-14 15:03:17 +0200

    Board

---
+++ firmware/apple/src/AppleBoard.java
@@ -0,0 +1,38 @@
+public class AppleBoard {
+    private String boardconfig;
+    private String platform;
+    private Long cpid;
+    private Long bdid;
+
+    public String getBoardConfig() {
+        return boardconfig;
+    }
+
+    public void setBoardConfig(String boardconfig) {
+        this.boardconfig = boardconfig;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public Long getCpid() {
+        return cpid;
+    }
+
+    public void setCpid(Long cpid) {
+        this.cpid = cpid;
+    }
+
+    public Long getBdid() {
+        return bdid;
+    }
+
+    public void setBdid(Long bdid) {
+        this.bdid = bdid;
+    }
+}
