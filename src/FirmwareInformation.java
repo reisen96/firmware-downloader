b179b75c520ea357e45331bc7db9aa0693a5be2f
roee <reisenr96@gmail.com>
2023-02-15 14:52:37 +0200

    Device list Working

--- src/FirmwareInformation.java
+++ src/FirmwareInformation.java
@@ -1,3 +1,7 @@
+import com.google.gson.Gson;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
@@ -14,12 +18,17 @@ public class FirmwareInformation {
 
     private HttpClient infoClient;
 
+    private Gson gson;
+
     public FirmwareInformation() throws MalformedURLException {
         apiURL = new URL("https://api.ipsw.me/v4");
         infoClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).build();
+        gson = new Gson();
     }
 
     public ArrayList<AppleDevice> getDeviceList() throws IOException, InterruptedException {
+        AppleDevice appleDevice;
+        JSONArray deviceListJson;
         ArrayList<AppleDevice> deviceList = new ArrayList<>();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://api.ipsw.me/v4/devices"))
@@ -27,11 +36,11 @@ public class FirmwareInformation {
                 .GET()
                 .build();
         HttpResponse<String> response = infoClient.send(request, HttpResponse.BodyHandlers.ofString());
-        
-
-
-
-
+        deviceListJson = new JSONArray(response.body());
+        for (Object deviceJson : deviceListJson) {
+            appleDevice = gson.fromJson(deviceJson.toString(), AppleDevice.class);
+            deviceList.add(appleDevice);
+        }
         return deviceList;
     }
 }
