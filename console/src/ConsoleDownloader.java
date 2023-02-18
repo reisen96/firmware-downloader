b837749e38d3399942766beeebee2c9ea7dcc581
roee <reisenr96@gmail.com>
2023-02-18 13:01:09 +0200

    Menu #1

--- console/src/ConsoleDownloader.java
+++ console/src/ConsoleDownloader.java
@@ -2,6 +2,13 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 
 public class ConsoleDownloader {
+    private enum MenuOption {
+        DEVICELIST,
+        FIRMWARELIST,
+        IDENTIFIER,
+        DOWNLOAD,
+        DOWNLOADLIST
+    }
 
     private final FirmwareInformation firmwareInformation;
     private final FirmwareDownloader firmwareDownloader;
@@ -13,9 +20,10 @@ public class ConsoleDownloader {
 
     public void mainMenu() {
 
+
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         ConsoleDownloader consoleDownloader = new ConsoleDownloader();
         consoleDownloader.mainMenu();
     }
--- src/FirmwareDownloader.java
+++ src/FirmwareDownloader.java
@@ -1,16 +1,13 @@
 import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.http.HttpClient;
 import java.util.ArrayList;
 
 public class FirmwareDownloader {
 
-    private final HttpClient downloadClient;
     private final ArrayList<Download> downloadList;
     private final String defaultDownloadDestination;
 
     public FirmwareDownloader() {
-        downloadClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).build();
         downloadList = new ArrayList<>();
         defaultDownloadDestination = System.getProperty("user.home" + "Downloads");
     }
