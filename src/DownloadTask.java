ba8242520f4320eed6e56a09fb33b7a899655a48
roee <reisenr96@gmail.com>
2023-02-23 18:59:01 +0200

    Cancel download #2

---
+++ src/DownloadTask.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+
+public class DownloadTask implements Runnable {
+
+    private final Download download;
+    private Thread downloaderThread;
+
+    public DownloadTask(Download download, String destination) {
+        download.setDestination(destination + download.getFileName());
+        this.download = download;
+    }
+
+    @Override
+    public void run() {
+        downloaderThread = Thread.currentThread();
+        download.setId(downloaderThread.threadId());
+        try (FileOutputStream downloadedFile = new FileOutputStream(download.getFileName())) {
+            HttpURLConnection httpConnection = (HttpURLConnection) (download.getUrl().openConnection());
+            BufferedInputStream inputStream = new BufferedInputStream(httpConnection.getInputStream());
+            BufferedOutputStream outputStream = new BufferedOutputStream(downloadedFile, 1024);
+            byte[] downloadedBytes = new byte[1024];
+            int completeFileSize = httpConnection.getContentLength(), currentFileSize = 0, currentlyRead;
+            while ((currentlyRead = inputStream.read(downloadedBytes, 0, 1024)) >= 0) {
+                currentFileSize += currentlyRead;
+                download.setProgress((double) currentFileSize / (double) completeFileSize);
+                outputStream.write(downloadedBytes, 0, currentlyRead);
+            }
+            download.setStatus(Download.DownloadStatus.COMPLETED);
+            downloadedFile.close();
+            inputStream.close();
+            outputStream.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void stop() {
+        File incompleteFile;
+        downloaderThread.interrupt();
+        incompleteFile = new File(download.getDestination() + download.getFileName());
+        incompleteFile.delete();
+        download.setStatus(Download.DownloadStatus.CANCELED);
+    }
+}
--- src/FirmwareDownloader.java
+++ src/FirmwareDownloader.java
@@ -1,47 +1,24 @@
-import java.io.*;
-import java.net.HttpURLConnection;
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.Stack;
+import java.util.TreeMap;
 
 public class FirmwareDownloader {
 
     private final ArrayList<Download> downloadList;
+    private final TreeMap<Long, DownloadTask> downloadTasks;
     private final String defaultDownloadDestination;
 
     public FirmwareDownloader() {
         downloadList = new ArrayList<>();
+        downloadTasks = new TreeMap<>();
         defaultDownloadDestination = System.getProperty("user.home") + "/Downloads";
     }
 
     public void downloadFirmware(AppleFirmware firmwareToDownload, String downloadDestination) {
         String destination = downloadDestination == null ? defaultDownloadDestination : downloadDestination;
         Download newDownload = new Download(firmwareToDownload);
+        Thread downloadThread = new Thread(new DownloadTask(newDownload, destination));
         downloadList.add(newDownload);
-        Runnable download = () ->
-        {
-            try (FileOutputStream downloadedFile = new FileOutputStream(newDownload.getFileName())) {
-                HttpURLConnection httpConnection = (HttpURLConnection) (newDownload.getUrl().openConnection());
-                BufferedInputStream inputStream = new BufferedInputStream(httpConnection.getInputStream());
-                BufferedOutputStream outputStream = new BufferedOutputStream(downloadedFile, 1024);
-                byte[] downloadedBytes = new byte[1024];
-                int completeFileSize = httpConnection.getContentLength(), currentFileSize = 0, currentlyRead;
-                newDownload.setId(Thread.currentThread().threadId());
-                newDownload.setDestination(destination);
-                while ((currentlyRead = inputStream.read(downloadedBytes, 0, 1024)) >= 0) {
-                    currentFileSize += currentlyRead;
-                    newDownload.setProgress((double) currentFileSize / (double) completeFileSize);
-                    outputStream.write(downloadedBytes, 0, currentlyRead);
-                }
-                newDownload.setStatus(Download.DownloadStatus.COMPLETED);
-                downloadedFile.close();
-                inputStream.close();
-                outputStream.close();
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
-        };
-        Thread downloadThread = new Thread(download);
         downloadThread.start();
     }
 
@@ -54,13 +31,7 @@ public class FirmwareDownloader {
     }
 
     public void cancelDownload(Long downloadID) {
-        for (Download download : downloadList) {
-            if (download.getId().equals(downloadID)) {
-                // Download threads!!!
-                download.setStatus(Download.DownloadStatus.CANCELED);
-                return;
-            }
-        }
+        downloadTasks.get(downloadID).stop();
     }
 
     public void clearDownloadList() {
